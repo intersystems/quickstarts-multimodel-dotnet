68fc2e57363fd3ec62ab45e504959ea0fb6897c8
DuongDo-InterSystems <Duong.Do@intersystems.com>
2018-12-19 14:28:09 -0500

    Add .NET multimodel QS code

---
+++ quickstarts-multimodel-dotnet/Airport.cs
@@ -0,0 +1,54 @@
+﻿/* 
+* PURPOSE: This class is used by multimodelQS.cs to get airport infromation 
+*/
+
+using System;
+using id = InterSystems.XEP.Attributes.Id;
+
+namespace Demo
+{
+    class Airport
+    {
+        [id(generated = false)]
+        private String code;
+        private String name;
+        private Location location;
+
+
+        public Airport()
+        {
+
+        }
+
+        public Airport(String name, String code, Location location)
+        {
+            this.name = name;
+            this.code = code;
+            this.location = location;
+        }
+        public String getName()
+        {
+            return name;
+        }
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+        public String getCode()
+        {
+            return code;
+        }
+        public void setCode(String code)
+        {
+            this.code = code;
+        }
+        public Location getLocation()
+        {
+            return location;
+        }
+        public void setLocation(Location location)
+        {
+            this.location = location;
+        }
+    }
+}
